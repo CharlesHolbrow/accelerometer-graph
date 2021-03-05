84fa2d64d9bbdd6a0a1af220bcc5fa30638ef4bc
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-03-05 17:46:40 -0500

    react tutorial complete

--- src/index.js
+++ src/index.js
@@ -2,23 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        { this.props.value }
-      </button>
-    );
-  }
+function Square (props) {
+  return (
+    <button
+      className="square"
+      onClick= { props.onClick }
+    >
+      { props.value }
+    </button>
+  )
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: new Array(9).fill(null),
+      xIsNext: false,
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    if (calculateWinner(this.state.squares) || this.state.squares[i]) return;
+    const xIsNext = !this.state.xIsNext
+    const squares = this.state.squares.slice()
+    squares[i] = xIsNext ? '✅' : '❌'
+    this.setState({ squares, xIsNext })
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? '✅' : '❌'}`;
+    }
 
     return (
       <div>
@@ -57,6 +87,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
