73f47494cf055dcd24ca5113560ac2902437a7ca
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-03-05 19:39:20 -0500

    playing with apexcharts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square (props) {
   return (
     <button
-      className="square"
+      className='square'
       onClick= { props.onClick }
     >
       { props.value }
@@ -13,7 +14,7 @@ function Square (props) {
   )
 }
 
-class Board extends React.Component {
+class Board extends Component {
 
   constructor(props) {
     super(props)
@@ -52,18 +53,18 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className='status'>{status}</div>
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -73,14 +74,14 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Game extends Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
@@ -109,9 +110,117 @@ function calculateWinner(squares) {
   return null;
 }
 
+const options = {
+  chart: {
+    id: 'realtime',
+    height: 350,
+    type: 'line',
+    animations: {
+      enabled: true,
+      easing: 'linear',
+      dynamicAnimation: {
+        speed: 1000
+      }
+    },
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Dynamic Updating Chart',
+    align: 'left'
+  },
+  markers: {
+    size: 0
+  },
+  xaxis: {
+    type: 'datetime',
+    range: 100,
+  },
+  yaxis: {
+    max: 100,min: 0
+
+  },
+  legend: {
+    show: false
+  },
+};
+
+
+
+class Charty extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      options: {
+        chart: {
+          id: 'basic-line',
+          animations: { enabled: false, easing: 'linear', dynamicAnimation: { speed: 1000 }, },
+          toolbar: { show: false },
+          zoom: { enabled: false },
+        },
+        stroke: { curve: 'smooth' },
+        // xaxis: {
+        //   type: 'datetime',
+        //   range: 100,
+        // },
+        yaxis: { max: 100, min: 0 },
+        dataLabels: { enabled: false }
+      },
+      series: [
+        {
+          name: 'series-1',
+          data: new Array(100).fill(50)
+        }
+      ],
+      interval: setInterval(() => {
+        const i = this.state.series[0].data.length - 1
+        const currentValue = this.state.series[0].data[i]
+        const newValue = currentValue + (Math.random() * 5 - 2.5)
+        this.push(Math.max(Math.min(newValue, 100)), 0)
+      }, 250)
+    };
+  }
+
+  render() {
+    return (
+      <div className='line-chart'>
+        <Chart
+          options={this.state.options}
+          series={this.state.series}
+          type='line'
+          width='500'
+          height='500'
+        />
+      </div>
+    )
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.state.interval)
+  }
+
+  push(value) {
+    const data = this.state.series[0].data.slice()
+    data.push(value)
+    data.shift()
+    const series = [{ data, name: 'series-1' }]
+    this.setState({ series })
+  }
+}
+
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Charty />,
   document.getElementById('root')
 );
