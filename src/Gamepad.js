15c0c51486bea3d93e0fa13118ffee938e0f9bfc
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-03-07 17:55:06 -0500

    Gamepad makes sense

--- src/Gamepad.js
+++ src/Gamepad.js
@@ -1,68 +1,89 @@
 import React from 'react'
 
-function PadButton({value}) {
+const getGamepadsInObject = () => {
+  // Firefox returns an Array, Chrome returns an object with int for keys
+  const gamePads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : [])
+  if (Array.isArray(gamePads)) {
+    const obj = {}
+    gamePads.forEach((pad, key) => { obj[key] = pad })
+    return obj
+  }
+  return gamePads
+}
+
+const jsPadsToCPads = (jsPadObj) => {
+  const cPadObj = {}
+  Object.entries(jsPadObj).forEach(([i, pad]) => { if (pad) cPadObj[i] = createCPad(pad) })
+  return cPadObj
+}
+
+function PadButton({value, handle, time}) {
   return <span>{value ? '✅' : '⭕️'}</span>
 }
 
 function Pad({buttons}) {
-  const elements = buttons.map((b, i) => <PadButton value={b.value} key={i} />)
+  const elements = buttons.map((b, i) => {
+    return <PadButton value={b} key={i} />
+  })
+
   return (
     <div>{elements}</div>
   )
 }
 
+const createCPad = (jsPad) => {
+  return { buttons: jsPad.buttons.map(b => b.value), id: jsPad.id, index: jsPad.index }
+}
+
 function Pads({ time }) {
-  const [padState, setPadState] = React.useState({})
+  const previousGamepadsRef = React.useRef([])
 
-  React.useEffect(() => {
-    console.log('  pads component useEffect run')
-    const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-    console.log('  found pads:', gamepads)
-    for (const pad of gamepads) {
-      if (!pad) continue
-      setPadState(oldPadState => {
-        const newPadState = { ...oldPadState }
-        newPadState[pad.index] = pad
-        return newPadState
-      })
+  // Note that firefox will return the same object on subsequent calls to
+  // getGamepads. The immutable properties will have changed, but the objects
+  // will be the same. If we want to compare objects between callbacks, we
+  // must deep copy any properties we are interested in.
+  const jsGamepads = getGamepadsInObject()
+  const currentGamepads = jsPadsToCPads(jsGamepads)
+  const previousGamepads = previousGamepadsRef.current
+  previousGamepadsRef.current = currentGamepads
+
+  // cStateRef stores the state of all current game pads. They keys are .index
+  // values, and values are custom objects created with 'createCPad(jsObject)'.
+  // Unlike currentGamepads and previousGamepads, a change to the cState.buttons
+  // object between frames should indicate a change to the underlying data.
+  const cStateRef = React.useRef({})
+
+  const addPad = (cPad) => cStateRef.current[cPad.index] = createCPad(cPad)
+  const removePad = (cPad) => delete cStateRef.current[cPad.index]
+
+  // check if we need to remove pads
+  Object.entries(previousGamepads).forEach(([i, cPad]) => {
+    if (cPad && !currentGamepads[i]){
+      removePad(cPad)
+      console.log(`pad${i} removed`)
     }
-
-    const onConnect = (event) => {
-      console.log('   pad connected:', event.gamepad)
-      setPadState(oldPadState => {
-        const newPadState = {...oldPadState }
-        newPadState[event.gamepad.index] = event.gamepad
-        return newPadState
-      })
+  })
+  // check if we need to add pads
+  Object.entries(currentGamepads).forEach(([i, cPad]) => {
+    if (!cPad) return
+    if (!previousGamepads[i]) {
+      console.log(`pad${i} added`, jsGamepads[i])
+      addPad(cPad)
+    } else {
+      for (const [j, b] of cPad.buttons.entries()) {
+        if (b !== previousGamepads[i].buttons[j]) console.log(`pad${i} b${j} state: ${b} at ${time}`)
+      }
+      cStateRef.current[cPad.index] = cPad
     }
-  
-    const onDisconnect = (event) => {
-      console.log('  pad disconnected:', event.gamepad)
-      setPadState(oldPadState => {
-        const newPadState = {...oldPadState}
-        delete newPadState[event.gamepad.index]
-        return newPadState
-      })
-    }
+  })
 
-    window.addEventListener('gamepadconnected', onConnect)
-    window.addEventListener('gamepaddisconnected', onDisconnect)
-
-    return () => {
-      window.removeEventListener('gamepadconnect', onConnect)
-      window.removeEventListener('gamepaddisconnect', onDisconnect)
-    }
-  }, [])
-
-  const pads = Object.entries(padState).map(([index, val]) => {
-
-    const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-    return  <Pad buttons={gamepads[index].buttons} key={index}/>
+  const padElements = Object.entries(cStateRef.current).map(([i, cPad]) => {
+    return <Pad buttons={cPad.buttons} key={i} ></Pad>
   })
 
   return (
-    <div> pads {time}
-      {pads}
+    <div>Pads
+      {padElements}
     </div>
   )
 }
