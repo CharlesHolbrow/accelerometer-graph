73e33a2dacb364f5ea44e8b4960c0061facfaa49
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-03-15 13:30:38 -0400

    organize to get ready for recording

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ import MotionMaster from './MotionMaster'
 // My miscellaneous
 import { sendToServer } from './server-access'
 
+// Find which items we need to remove from a sequence of events. Assume each
+// event has a .x member which indicates it's time.
+const indexOfLastEventBefore = (time, events) => {
+  let i = 0
+  while (i < events.length) {
+    if (events[i].x > time) break
+    i++
+  }
+  return i
+}
+
+const SEND_INTERVAL_MS = 20000
+
 export default function App() {
   const motionEventsRef = React.useRef([]) // All motion events
   const buttonEventsRef = React.useRef([]) // All button events
@@ -23,70 +36,15 @@ export default function App() {
     gameTime: 0,
   })
 
-  // const [gameTime, setGameTime] = React.useState(0)
+  const [recording, setRecording] = React.useState(false)
+  const recordingStartTimeRef = React.useRef(0)
 
   // Use useRef for mutable variables that we want to persist
   // without triggering a re-render on their change
-  const requestRef = React.useRef()
-  const previousTimeRef = React.useRef()
+  const animationRef = React.useRef()
+  const timeRef = React.useRef(0)
+
   const graphDurationSeconds = 8
-
-  const animate = React.useCallback(time => {
-    time *= 0.001 // do everything in seconds, not milliseconds
-    if (typeof previousTimeRef.current === 'number') {
-      const deltaTime = time - previousTimeRef.current
-
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state
-
-      setState((oldState) => {
-        // If we want to "bail out", we can return the old state object
-        const {graphX, graphY, graphZ, buttons} = oldState
-
-        if (motionEventsRef.current.length) {
-          const newState = { gameTime: time } 
-
-          let i = motionEventsRef.current.length -1
-          const mostRecentEvent = motionEventsRef.current[i]
-          const previouslyAddedMotionEvent = previousMotionEvent.current
-          previousMotionEvent.current = mostRecentEvent
-
-          const graphStartTime = time - graphDurationSeconds
-          const sliceFrom = indexOfLastEventBefore(graphStartTime, graphX)
-          const sliceTo = graphX.length
-
-          newState.graphX = graphX.slice(sliceFrom, sliceTo)
-          newState.graphY = graphY.slice(sliceFrom, sliceTo)
-          newState.graphZ = graphZ.slice(sliceFrom, sliceTo)
-
-          if (mostRecentEvent !== previouslyAddedMotionEvent) {
-            newState.graphX.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.x })
-            newState.graphY.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.y })
-            newState.graphZ.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.z })
-          }
-
-          // find all the button pushes that are within the window of interest
-          const newButtons = newState.buttons = []
-          for (let i = buttonEventsRef.current.length - 1; i >=0; i--) {
-            const buttonEvent = buttonEventsRef.current[i]
-            if (buttonEvent.time < graphStartTime) break
-            if (buttonEvent.type === 'down') newButtons.push({x: buttonEvent.time, y: buttonEvent.button })
-          }
-
-          return newState
-        }
-        return oldState
-      })
-    }
-
-    previousTimeRef.current = time
-    requestRef.current = requestAnimationFrame(animate)
-  }, [])
-
-  React.useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate)
-    return () => { cancelAnimationFrame(requestRef.current) }
-  }, []) // eslint-disable-line
 
   const keyRef = React.useRef('secret')
   const setKey = (key) => keyRef.current = key
@@ -100,26 +58,72 @@ export default function App() {
     console.log(result)
   }
 
+  const toggleRecording = () => {
+    if (!recording) recordingStartTimeRef.current = state.gameTime
+    setRecording(!recording)
+  }
+
+  const animate = React.useCallback(time => {
+    time *= 0.001 // do everything in seconds, not milliseconds
+
+    const deltaTime = time - timeRef.current
+    timeRef.current = time
+
+    setState((oldState) => {
+      // If we want to "bail out", we can return the old state object
+      const {graphX, graphY, graphZ, buttons} = oldState
+      const newState = { gameTime: time }
+
+      if (motionEventsRef.current.length) {
+        let i = motionEventsRef.current.length -1
+        const mostRecentEvent = motionEventsRef.current[i]
+        const previouslyAddedMotionEvent = previousMotionEvent.current
+        previousMotionEvent.current = mostRecentEvent
+
+        const graphStartTime = time - graphDurationSeconds
+        const sliceFrom = indexOfLastEventBefore(graphStartTime, graphX)
+        const sliceTo = graphX.length
+
+        newState.graphX = graphX.slice(sliceFrom, sliceTo)
+        newState.graphY = graphY.slice(sliceFrom, sliceTo)
+        newState.graphZ = graphZ.slice(sliceFrom, sliceTo)
+
+        if (mostRecentEvent !== previouslyAddedMotionEvent) {
+          newState.graphX.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.x })
+          newState.graphY.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.y })
+          newState.graphZ.push({ x: mostRecentEvent.time, y: mostRecentEvent.acc.z })
+        }
+
+        // find all the button pushes that are within the window of interest
+        const newButtons = newState.buttons = []
+        for (let i = buttonEventsRef.current.length - 1; i >=0; i--) {
+          const buttonEvent = buttonEventsRef.current[i]
+          if (buttonEvent.time < graphStartTime) break
+          if (buttonEvent.type === 'down') newButtons.push({x: buttonEvent.time, y: buttonEvent.button })
+        }
+      }
+      return newState
+    })
+
+    animationRef.current = requestAnimationFrame(animate)
+  }, [])
+
+  React.useEffect(() => {
+    animationRef.current = requestAnimationFrame(animate)
+    return () => { cancelAnimationFrame(animationRef.current) }
+  }, []) // eslint-disable-line
+
   return (
     <div>
       <h3>App2</h3>
       <MotionMaster onMotionEvent={motion => motionEventsRef.current.push(motion) }/>
       <Graph dataX={state.graphX} dataY={state.graphY} dataZ={state.graphZ} dataB={state.buttons} />
-      <div>game time: {state.gameTime.toFixed(1)}</div>
+      <div>game time: {recording ? (state.gameTime - recordingStartTimeRef.current).toFixed(1) : 'X'}</div>
       <Pads time={state.gameTime} onButtonEvent={event => buttonEventsRef.current.push(event) }/>
       <LocalStoreTextField onChange={setSessionName} id='input-session-name' label='session' type='text' />
       <LocalStoreTextField onChange={setKey}         id='input-key'          label='key'     type='password' />
+      <button onClick={toggleRecording}>{recording ? 'Stop' : 'Record'}</button>
       <button onClick={sendMotionEventsToServer}>Send</button>
     </div>
   )
-}
-
-// Find which items we need to remove from the 
-const indexOfLastEventBefore = (time, events) => {
-  let i = 0
-  while (i < events.length) {
-    if (events[i].x > time) break
-    i++
-  }
-  return i
 }
